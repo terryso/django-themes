13c35b25bc9b5ae692668e149b82c8c5ace710a6
nee <nee@hoge.cn>
2017-11-23 21:06:01 +0800

    修复导入URL报错的问题

--- themes/urls.py
+++ themes/urls.py
@@ -1,9 +1,14 @@
-#try:
+# try:
 from django.conf.urls import patterns, url
-#except ImportError:
+# except ImportError:
 #    from django.conf.urls.defaults import patterns, url # Django < 1.4
 
-urlpatterns = patterns('themes.views',
+# urlpatterns = patterns('themes.views',
+    # url('^change/$', 'change', name='themes_change'),
+    # url('^change/(?P<theme_id>[\d]+)/$', 'change', name='themes_change_id'),
+# )
+
+urlpatterns = [
     url('^change/$', 'change', name='themes_change'),
     url('^change/(?P<theme_id>[\d]+)/$', 'change', name='themes_change_id'),
-)
+]
